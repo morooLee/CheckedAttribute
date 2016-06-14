e722f15c12e09ffae28b88b7b136eb39dfd91c28
이순한 <xellos82@hanmail.net>
2016-06-15 03:13:21 +0900

    2016-06-15_Desktop

--- CheckedAttribute/CheckedAttribute/MainWindow.xaml.cs
+++ CheckedAttribute/CheckedAttribute/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Abot.Crawler;
+using Abot.Poco;
+using System.Net;
+using log4net.Config;
 
 namespace CheckedAttribute
 {
@@ -27,7 +31,62 @@ namespace CheckedAttribute
 
         private void Apply_Button_Click(object sender, RoutedEventArgs e)
         {
+            //XmlConfigurator.Configure();
+            try
+            {
+                PoliteWebCrawler crawler = new PoliteWebCrawler();
+    
+                crawler.PageCrawlStartingAsync += crawler_ProcessPageCrawlStarting;
+                crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
+                crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowed;
+                crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed;
 
+                CrawlResult result = crawler.Crawl(new Uri(URL_TextBox.Text)); //This is synchronous, it will not go to the next line until the crawl has completed
+
+                if (result.ErrorOccurred)
+                {
+                    Console.WriteLine("Crawl of {0} completed with error: {1}", result.RootUri.AbsoluteUri, result.ErrorException.Message);
+                }
+                else
+                {
+                    Console.WriteLine("Crawl of {0} completed without error.", result.RootUri.AbsoluteUri);
+                }
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.ToString());
+            }           
+        }
+
+        void crawler_ProcessPageCrawlStarting(object sender, PageCrawlStartingArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            Console.WriteLine("About to crawl link {0} which was found on page {1}", pageToCrawl.Uri.AbsoluteUri, pageToCrawl.ParentUri.AbsoluteUri);
+        }
+
+        void crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+
+            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+                Console.WriteLine("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
+            else
+                Console.WriteLine("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
+
+            if (string.IsNullOrEmpty(crawledPage.Content.Text))
+                Console.WriteLine("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
+        }
+
+        void crawler_PageLinksCrawlDisallowed(object sender, PageLinksCrawlDisallowedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            Console.WriteLine("Did not crawl the links on page {0} due to {1}", crawledPage.Uri.AbsoluteUri, e.DisallowedReason);
+        }
+
+        void crawler_PageCrawlDisallowed(object sender, PageCrawlDisallowedArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            Console.WriteLine("Did not crawl page {0} due to {1}", pageToCrawl.Uri.AbsoluteUri, e.DisallowedReason);
         }
     }
 }
